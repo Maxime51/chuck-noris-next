032251ea3881707e30ab377ff5ed03de365d5707
mansuy maxime <mmansuy@norauto.fr>
2022-03-22 10:39:55 +0100

    maj

--- pages/404.tsx
+++
@@ -1,7 +0,0 @@
-import Layout from "../components/Layout"
-
-export default function Custom404() {
-  return <Layout>
-    <h1>404 - Page Not Found</h1>
-    </Layout>
-}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import Layout from '../components/Layout'
-
-
 export default function Home() {
   return (
     <div >
@@ -14,11 +10,6 @@ export default function Home() {
       </Head>
 
       <main  >
-        <Layout>
-          <div className="shadow-lg p-3 mb-5 bg-white rounded">
-          <img src="https://st.depositphotos.com/1005979/2976/i/600/depositphotos_29760945-stock-photo-quiz-red-3d-word-test.jpg" className="rounded mx-auto d-block" alt="..."></img>
-          </div>
-        </Layout>
       </main>
 
     </div>
